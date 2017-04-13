f67c5d97bffd93be7948d89bc7bff13430f1f3c1
henokbekele <henok2000@gmail.com>
2017-04-13 08:17:38 -0500

    crud operation done

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var lessMiddleware = require('less-middleware');
+var MongoClient = require('mongodb').MongoClient;
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -24,18 +25,85 @@ app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+//app.use('/', index);
 app.use('/users', users);
 
+app.get('/', function (req, res, next) {
+  res.render('add')
+})
+
+app.post('/add', function (req, res, nex) {
+  MongoClient.connect('mongodb://127.0.0.1:27017/orgDB', function (err, db) {
+    if (err) throw err;
+    var doc = {
+      name: req.body.name,
+      category: req.body.category,
+      loc: [req.body.longitude, req.body.latitude]
+    };
+    db.collection('orgCol').insert(doc, function (err, inserteddoc) {
+      if (err) throw err;
+      db.close();
+      res.send(JSON.stringify(inserteddoc.ops));
+    });
+  });
+})
+
+app.get('/search', function (req, res, next) {
+  MongoClient.connect('mongodb://127.0.0.1:27017/orgDB', function (err, db) {
+    if (err) throw err;
+    var query = { name: req.query.name }
+    console.log("queryyyyyy" + JSON.stringify(query))
+    db.collection('orgCol').find(query, function (err, searchdoc) {
+      if (err) throw err;
+      
+     searchdoc.toArray(function (err, arrydoc) {
+        if (err) throw err;
+        res.send((JSON.stringify(arrydoc)));
+      })
+db.close();
+    })
+  });
+})
+
+//update
+app.post('/update', function(req, res, next){
+  MongoClient.connect('mongodb://127.0.0.1:27017/orgDB', function(err, db){
+    if(err) throw err;
+    var query={name:req.body.name}
+    var operator={'$set':{category:req.body.category}}
+    db.collection('orgCol').update(query, operator,function(err, updoc){
+      if(err) throw err;
+
+       db.close();
+      res.send((JSON.stringify(updoc))+"Is updated in the collections")
+    })
+  })
+})
+
+app.post('/delete', function (req, res, next) {
+  MongoClient.connect('mongodb://127.0.0.1:27017/orgDB', function (err, db) {
+    if (err) throw err;
+    var query = { name: req.body.name }
+
+    db.collection('orgCol').remove(query, function (err, removeddoc) {
+      if (err) throw err;
+ 
+       db.close();
+      res.send((JSON.stringify(removeddoc))+"Is removed from the collections")
+
+    })
+  });
+})
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -45,4 +113,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(300, () => { console.log("server started") })
 module.exports = app;
